59853575d8b0cc475a7e51eec9a93ed22c7194ef
Yasmim Riberiro ✩ <159492044+Yasuribeiroo@users.noreply.github.com>
2025-03-06 11:21:02 -0300

    Add files via upload

---
+++ bem-vindo-a-eeep/components/Footer.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+
+function Footer () {
+  return (
+    <View>
+      <Text> 2024 EEEP Deputado Roberto Mesquita
+      </Text>
+    </View>
+  )
+}
+
+export default Footer
